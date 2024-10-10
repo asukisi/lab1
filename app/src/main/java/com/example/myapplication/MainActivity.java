bea14f95e88ee94074e9d501012c1895a419ab33
asukisi <zajcevaalina.776@gmail.com>
2024-10-10 04:08:07 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.view.View;
+import android.content.Intent;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+        public void openSecondActivity(View v) {
+        Intent intent = new Intent(this, MainActivity2.class);
+        String dataToSend = "Hello, Mokrinsky!";
+        intent.putExtra("testNameData", dataToSend);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -0,0 +1,27 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2); // замените на ваш файл макета
+
+        // Получаем данные из Intent
+        String data = getIntent().getStringExtra("testNameData");
+
+        // Находим TextView и устанавливаем текст
+        TextView myTextView = findViewById(R.id.myTextView); // Убедитесь, что у вас есть TextView с этим ID
+        if (data != null) {
+            myTextView.setText(data);
+        } else {
+            myTextView.setText("Нет данных");
+        }
+    }
+}
